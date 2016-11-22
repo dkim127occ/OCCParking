bab669231f2a0c18115c37edbf5f036dc02a4bed
jkloppenburg1 <jkloppenburg1@student.cccd.edu>
2016-11-21 16:02:25 -0800

    Added LotDDetailsActivity with its layouts, as well as landscape layouts for lots A and B

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dkim127/occparking/LotDDetailsActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.dkim127.occparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LotDDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lot_d_details);
+    }
+}
